0c51ef8c7774869eed459391af0f8d639523b8e5
Parikshit <parikshit.r2005@gmail.com>
2025-08-28 22:30:51 +0530

    declared routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
+import Homepage from './Components/Homepage/Homepage';
 
 function App() {
 
   return (
-    <div>homepage</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </Router>
   )
 }
 

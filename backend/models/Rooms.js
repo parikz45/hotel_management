5c8bc15d5a518f467df9c9bd92761d02f5e2ca69
Parikshit <parikshit.r2005@gmail.com>
2025-08-29 14:03:12 +0530

    defined rooms model

---
+++ backend/models/Rooms.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: {
+        type: String,   
+        required: true,
+        unique: true    
+    },
+    description: {  
+        type: [String]
+    },
+    capacity: {     
+        type: Number,
+        required: true
+    },
+    images: {      
+        type: [String]  
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('Room', roomSchema);
